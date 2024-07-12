5ab496c0e306f441a072099b2fbfb95b0483fee6
Junmao Lin <903716709junmao@gmail.com>
2024-07-11 20:02:36 -0400

    added logo and text to loading page

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, } from 'react-native';
+import { useFonts } from "expo-font";
+import logo from './images/TrawellLogo.png';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
+    <View style={styles.landingPage}>
+      <Image style={styles.landingLogo} source = {logo}/>
+      <Text style={styles.trawell}> Trawell </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  landingPage: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  landingLogo:{
+    height:250,
+    width:250
+  },
+
+  trawell:{
+    fontFamily: 'Comfortaa-Regular',
+    fontSize: 36
+  },
+
 });
